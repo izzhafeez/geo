49f2202cf0a052fb81a7f1027156b6604fb37604
Hafeez <izzhafeez@gmail.com>
2022-09-06 00:38:50 +0800

    removed structures

--- structures/comparable.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-from abc import abstractmethod
-from typing import Any, Protocol
-
-class Comparable(Protocol):
-    @abstractmethod
-    def __lt__(self: Any, other: Any) -> bool:
-        pass
