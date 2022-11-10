5e0193c96eb81deb8b0888003d291ec71ad0aac4
Hafeez <izzhafeez@gmail.com>
2022-11-10 12:37:35 +0800

    Edited ./setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 
 setup(
-  name='half_geo',
-  version='1.1.0',
+  name='halfgeo',
+  version='1.0.13',
   license='MIT',
   author="Izz Hafeez",
   author_email='izzhafeez@gmail.com',
@@ -13,12 +13,15 @@ setup(
   keywords='geospatial singapore',
   long_description=open('README.md').read(),
   long_description_content_type='text/markdown',
+  include_package_data=True,
   install_requires=[
       'pandas',
       'geopandas',
       'numpy',
       'shapely',
-      'gsheets'
+      'gsheets',
+      'Pillow',
+      'fiona'
     ],
 
 )
